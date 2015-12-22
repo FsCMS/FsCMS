259a2d39dabd8598f3274d3a4e9ef15a925910ed
Jerold Haas <jeroldhaas@gmail.com>
2015-12-22 05:12:01 -0500

    first set of files for project

---
+++ DevNotes.md
@@ -0,0 +1,3 @@
+# Development Notes #
+
+- [UI.Next Tutorial](https://github.com/intellifactory/websharper.ui.next/blob/master/docs/Tutorial.md)
---
+++ Schema.fs
@@ -0,0 +1,49 @@
+namespace FsCms
+
+open System
+open System.Collections.Generic
+
+module Schema = 
+    type PropertyType = 
+        { Name : string
+          Deleted : bool }
+    
+    // textareas, checkboxes, etc. for node
+    type Property = 
+        { Type : PropertyType
+          Value : string
+          Deleted : bool }
+    
+    type AttributeType = 
+        { Type : string
+          Deleted : bool }
+    
+    // for non-property values
+    type Attribute = 
+        { Type : AttributeType
+          Value : string
+          Deleted : bool }
+    
+    // node can be page, blog, etc.
+    type Node = 
+        { Properties : Property list
+          Attributes : Attribute list
+          Deleted : bool }
+    
+    type AuditedItem = 
+        | Node
+        | Property
+        | PropertyType
+        | Attribute
+        | AttributeType
+    
+    type AuditedAction = 
+        | Create
+        | Modify
+        | Delete
+    
+    type AuditLog = 
+        { Item : AuditedItem
+          Action : AuditedAction
+          //    User: User
+          TimeStamp : DateTime }
