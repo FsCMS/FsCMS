00112e36a5daafcff12d4f1c9a1c804a801f9114
Marshall Bowers <elliott.codes@gmail.com>
2017-09-23 00:56:27 -0400

    Add projects

---
+++ src/FsCms.Core/Schema.fs
@@ -0,0 +1,57 @@
+module FsCms.Core.Schema
+
+open System
+
+type PropertyType =
+    { Name : string
+      Deleted : bool }
+
+// textareas, checkboxes, etc. for node
+type Property =
+    { Type : PropertyType
+      Value : string
+      Deleted : bool }
+
+type AttributeType =
+    { Type : string
+      Deleted : bool }
+
+// for non-property values
+type Attribute =
+    { Type : AttributeType
+      Value : string
+      Deleted : bool }
+
+// node can be page, blog, etc.
+type Node =
+    { Properties : Property list
+      Attributes : Attribute list
+      Deleted : bool }
+
+type Privilege = Privilege of string
+
+type Role =
+    { RoleName : string
+      Privileges : Privilege list }
+
+type User =
+    { Username : string
+      Password : string }
+
+type AuditedItem =
+    | Node
+    | Property
+    | PropertyType
+    | Attribute
+    | AttributeType
+
+type AuditedAction =
+    | Create
+    | Modify
+    | Delete
+
+type AuditLog =
+    { Item : AuditedItem
+      Action : AuditedAction
+      //    User: User
+      TimeStamp : DateTime }
---
+++ src/FsCms.Server.Suave/Program.fs
@@ -0,0 +1,20 @@
+﻿module FsCms.Server.Suave
+
+open System
+open System.Threading
+open Suave
+
+[<EntryPoint>]
+let main argv =
+    let cts = new CancellationTokenSource()
+    let config = {
+        defaultConfig with cancellationToken = cts.Token }
+    let listening, server = startWebServerAsync config (Successful.OK "Hello World")
+
+    Async.Start (server, cts.Token)
+    printfn "Make requests now"
+    Console.ReadKey true |> ignore
+
+    cts.Cancel()
+
+    0
